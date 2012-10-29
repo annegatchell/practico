79474052ee37c16d687479ec2b4d28acecffc1cc
Anne Gatchell <annegatchell@gmail.com>
2012-10-29 13:07:07 -0600

    Implemented insertion sort

---
+++ insertionSort.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+
+void swap(int*a, int*b){
+	int tmp;
+	tmp = *b;
+	*b = *a;
+	*a = tmp;
+}
+
+void insertionSort(int* s, int n){
+	int i, j;
+
+	for(i = 0; i < n; i++){
+		j = i;
+		while((j>0) && (s[j] < s[j-1])){
+			printf("%d, %d\n", s[j], s[j-1]);
+			swap(&s[j], &s[j-1]);
+			j = j-1;
+		}
+	}
+	return;
+}
+
+int main(int argc, char*argv[]){
+	int a[] = {3,1,3,2,8,4,6,9};
+	//printf("%ld\n", sizeof(a)/sizeof(int));
+	insertionSort(a, sizeof(a)/sizeof(int));
+	int i;
+	for (i=0; i < sizeof(a)/sizeof(int); i++){
+		printf("%d\n", a[i]);
+	}
+	return 1;
+}
